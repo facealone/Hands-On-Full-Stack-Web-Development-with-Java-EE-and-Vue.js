77049022588ff2ddba1cdaf66f9eb3254dec08c5
Daniel Pelaez <estigma88@gmail.com>
2019-05-23 19:48:20 -0500

    Updating for debbuging

--- CH1/startwithvuejs/src/components/HelloWorld.vue
+++ CH1/startwithvuejs/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ defineMessage }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -37,6 +37,10 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+  
+  get defineMessage () {
+    return this.msg + ", yeeeaahhh"
+  }
 }
 </script>
 
