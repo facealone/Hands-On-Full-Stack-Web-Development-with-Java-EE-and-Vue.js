9ea8c4c14094d88b0d65b1ef50f9644ecc890d5c
Daniel Pelaez <estigma88@gmail.com>
2019-07-15 19:59:47 -0500

    Changing database script

--- CH3/StartWithJEE8/StartWithJEE8-ejb/src/main/resources/database.sql
+++ CH3/StartWithJEE8/StartWithJEE8-ejb/src/main/resources/database.sql
@@ -1,3 +1,5 @@
+USE `packtmysql`;
+
 CREATE TABLE `DELIVERY` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `address` varchar(100) NOT NULL,
@@ -7,6 +9,23 @@ CREATE TABLE `DELIVERY` (
   `email` varchar(100) NOT NULL,
   `state` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `USER_DATA` (
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `FOOD_SERVICE` (
+  `email` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `food_type` varchar(100) NOT NULL,
+  `delivery_fee` int(11) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`email`),
+  CONSTRAINT `FOOD_SERVICE_FK` FOREIGN KEY (`email`) REFERENCES `USER_DATA` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `FOOD_PRODUCT` (
@@ -22,17 +41,6 @@ CREATE TABLE `FOOD_PRODUCT` (
   CONSTRAINT `FOOD_PRODUCT_FK` FOREIGN KEY (`food_service`) REFERENCES `FOOD_SERVICE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `FOOD_SERVICE` (
-  `email` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `food_type` varchar(100) NOT NULL,
-  `delivery_fee` int(11) NOT NULL,
-  `active` tinyint(1) NOT NULL,
-  PRIMARY KEY (`email`),
-  CONSTRAINT `FOOD_SERVICE_FK` FOREIGN KEY (`email`) REFERENCES `USER_DATA` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 CREATE TABLE `ITEM` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `food_product` int(11) NOT NULL,
@@ -43,10 +51,4 @@ CREATE TABLE `ITEM` (
   KEY `ITEM_FK_1` (`food_product`),
   CONSTRAINT `ITEM_FK` FOREIGN KEY (`delivery`) REFERENCES `DELIVERY` (`id`),
   CONSTRAINT `ITEM_FK_1` FOREIGN KEY (`food_product`) REFERENCES `FOOD_PRODUCT` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `USER_DATA` (
-  `email` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
